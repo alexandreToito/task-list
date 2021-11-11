3826259783ff665e34a6da9fb1bd244dc24a77f8
Alexandre Toito <alexandretoito@gmail.com>
2021-11-11 20:31:34 -0300

    Challenge resolution

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,24 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle) {
+      setTasks([...tasks, { id: Math.random(), title: newTaskTitle, isComplete: false }])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasksCompletion = tasks.map(task => {
+      if(task.id === id) task.isComplete = !task.isComplete
+      return task
+    })
+    setTasks(updatedTasksCompletion)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const remainTasks = tasks.filter(task => task.id !== id)
+    setTasks(remainTasks)
   }
 
   return (
@@ -32,9 +42,9 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
@@ -50,7 +60,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -66,7 +76,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
